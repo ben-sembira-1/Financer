9b634d7d9335bf493a04ac0159a5cf2c028585b8
Ben Sembira <ben.sembira.1@gmail.com>
2024-10-22 14:50:53 +0300

    fix: align content to top

--- financer-frontend/src/pages/MainPage/index.tsx
+++ financer-frontend/src/pages/MainPage/index.tsx
@@ -1,7 +1,7 @@
 import { Typography, Stack } from "@mui/material";
 export function MainPage() {
 	return (
-		<Stack>
+		<Stack height={"100vh"} justifyContent={"start"}>
 			<Typography variant="h1">Financer</Typography>
 			<Typography variant="h2">Monthly Tracker</Typography>
 		</Stack>
