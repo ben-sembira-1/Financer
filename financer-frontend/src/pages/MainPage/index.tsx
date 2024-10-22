07691b7aa63a0832a2f6a561e5a48eb44830fdd8
Ben Sembira <ben.sembira.1@gmail.com>
2024-10-22 14:45:22 +0300

    feat: basic page with MUI

---
+++ financer-frontend/src/pages/MainPage/index.tsx
@@ -0,0 +1,9 @@
+import { Typography, Stack } from "@mui/material";
+export function MainPage() {
+	return (
+		<Stack>
+			<Typography variant="h1">Financer</Typography>
+			<Typography variant="h2">Monthly Tracker</Typography>
+		</Stack>
+	);
+}
