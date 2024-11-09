661115e3a7e250fc76dfa9d4346b85257928dcfb
Ben <ben.sembira.dev@gmail.com>
2024-11-09 08:31:40 +0200

    remove my old attempt

--- financer-frontend/src/pages/MainPage/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography, Stack } from "@mui/material";
-export function MainPage() {
-	return (
-		<Stack height={"100vh"} justifyContent={"start"}>
-			<Typography variant="h1">Financer</Typography>
-			<Typography variant="h2">Monthly Tracker</Typography>
-		</Stack>
-	);
-}
